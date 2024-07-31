d7bb2920243f31d04e35ce6d1e5ffb851f61a09d
CamilaDiazToledo <camiditol1906@gmail.com>
2024-07-31 12:28:11 -0500

    feat: :sparkles: Realizacion de la Funcion Modificacion
    Realizacion de la Funcion Modificacion que consiste en la modificacion de los campos de elementos ya existentes dentro del json
    
    @CamilaToledo

--- Modulos/Registro_y_edicion_de_info.py
+++ Modulos/Registro_y_edicion_de_info.py
@@ -41,16 +41,44 @@ def registro_ciudades():
 
 
 
-registro_ciudades()
+#registro_ciudades()
+
+
+def modificacion_ciudades():
+    while True:
+        try:
+            data = cargar_datos("ciudades.json", "d")
+
+            nombre_ciudad = input('Ingrese el nombre de la ciudad que desea modificar: \n ')
+            if data.get(nombre_ciudad,None) != None:
+                criterios = ['codigo postal','poblacion','pais']
+                n = 0
+                for i in criterios:
+                    n = n+1
+                    print(n,i)
+                eleccion = int(input('Digite el número de la opción que desea elegir: \n '))
+                eleccion = eleccion-1
+                elegido = criterios[eleccion]
+                criterio = str(elegido)
+                cambio= input(f'Ingrese los nuevos datos a cambiar para {nombre_ciudad }, en el campo de {criterio}: \n')
+                validacion = int(input(f'Esta seguro que desea guardar la siguiente informacion \n {cambio} en el campo de {criterio} \n1.SI \n2.NO \n:'))
+                while True:
+                    if validacion == 1:
+                        data[nombre_ciudad][criterio] = cambio
+                        guardar_datos(data,"ciudades.json")
+                        break
+                    elif validacion == 2:
+                        print("Usted selecciono 2.NO, por favor reinicie el proceso")
+                        break
+                    elif validacion != 1 or 2:
+                        print('Numero no valido')
+
+            else:
+                print("Resultado no apto para modificacion de campo...")
+        except Exception as e:
+            print(f"Ocurrió un error: {e}")
 
 
 
 
-
-
-
-
-
-
-
-
+modificacion_ciudades()
